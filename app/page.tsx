efc7792f582f8d69721b690560fa42e803f152a5
gregoryjohsonjg6 <gregoryjohsonjg6@gmail.com>
2023-10-19 04:56:43 -0400

    feat: done

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,342 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useCountries } from "use-react-countries";
+import {
+  Card,
+  Input,
+  Button,
+  Typography,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+} from "@material-tailwind/react";
+
+import { classNames } from "./lib";
+
+type Inputs = {
+  username: string;
+  email: string;
+  phone: string;
+  country: string;
+  password?: string;
+  passwordConfirm?: string;
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [step, setStep] = useState(1);
+  const { countries } = useCountries();
+  const [country, setCountry] = useState(0);
+  const [phoneCountry, setPhoneCountry] = useState(0);
+  const { name, flags, countryCallingCode } = countries[phoneCountry];
+  const countryName = countries[country];
+  const [isEnabled, setIsEnabled] = useState(true);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const password = watch('password');
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const onSubmit = () => {
+    if (step === 1 ) {
+      setStep(2);
+    } else if (step === 2) {
+      setStep(3);
+    }
+  }
+
+  const renderStep1 = () => (
+    <div
+      className="mb-1 flex flex-col gap-6"
+    >
+      <Typography
+        variant="h6"
+        color="blue-gray"
+        className="-mb-3 text-white"
+      >
+        Username
+      </Typography> 
+      <Input
+        size="lg"
+        placeholder="Input username"
+        className="focus:!border-primary"
+        labelProps={{
+          className: "before:content-none after:content-none",
+        }}
+        containerProps={{
+          className: "bg-white rounded-lg",
+        }}
+        {...register("username", { required: {value: true, message: 'Username is required'}, minLength: {value: 4, message:'Username must be at least 4 characters long'}, maxLength: {value: 12, message: 'Username must be at most 12 characters long'} })}
+      />
+      {errors.username && <span className="text-red-700">{errors.username.message}</span>}
+      <Typography
+        variant="h6"
+        color="blue-gray"
+        className="-mb-3 text-white"
+      >
+        Email
+      </Typography>
+      <Input
+        size="lg"
+        placeholder="Input email"
+        className="focus:!border-primary"
+        labelProps={{
+          className: "before:content-none after:content-none",
+        }}
+        containerProps={{
+          className: "bg-white rounded-lg",
+        }}
+        type="email"
+        {...register("email", { required: {value: true, message: 'Email is required'} })}
+      />
+      {errors.email && <span className="text-red-700">{errors.email.message}</span>}
+      <Typography
+        variant="h6"
+        color="blue-gray"
+        className="-mb-3 text-white"
+      >
+        Phone number
+      </Typography>
+      <div className="relative flex w-full max-w-[24rem]">
+        <Menu placement="bottom-start">
+          <MenuHandler>
+            <Button
+              ripple={false}
+              variant="text"
+              color="blue-gray"
+              className="flex h-10 items-center gap-2 rounded-r-none border border-r-0 border-blue-gray-200 bg-white focus:!bg-white pl-3"
+            >
+              <img
+                src={flags.svg}
+                alt={name}
+                className="h-4 w-4 rounded-full object-cover"
+              />
+              {countryCallingCode}
+            </Button>
+          </MenuHandler>
+          <MenuList className="max-h-[20rem] max-w-[18rem]">
+            {countries.map(
+              ({ name, flags, countryCallingCode }, index) => {
+                return (
+                  <MenuItem
+                    key={name}
+                    value={name}
+                    className="flex items-center gap-2"
+                    onClick={() => setPhoneCountry(index)}
+                  >
+                    <img
+                      src={flags.svg}
+                      alt={name}
+                      className="h-5 w-5 rounded-full object-cover"
+                    />
+                    {name}{" "}
+                    <span className="ml-auto">
+                      {countryCallingCode}
+                    </span>
+                  </MenuItem>
+                );
+              }
+            )}
+          </MenuList>
+        </Menu>
+        <Input
+          type="tel"
+          placeholder="Mobile Number"
+          className="rounded-l-none focus:!border-transparent"
+          labelProps={{
+            className: "before:content-none after:content-none",
+          }}
+          containerProps={{
+            className: "min-w-0 bg-white rounded-lg rounded-l-none",
+          }}
+          {...register("phone", { required: {value:true, message: 'Phone number is required'}, pattern: {
+            value: /^[0-9]{10}$/, // Regex pattern for a 10-digit phone number
+            message: "Invalid phone number"
+          } })}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {errors.phone && <span className="text-red-700">{errors.phone.message}</span>}
+      <Typography
+        variant="h6"
+        color="blue-gray"
+        className="-mb-3 text-white"
+      >
+        Country
+      </Typography>
+      <Menu placement="bottom-start">
+        <MenuHandler>
+          <Input
+            type="tel"
+            placeholder="Mobile Number"
+            className="focus:!border-transparent"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+            value={countryName.name}
+            containerProps={{
+              className: "min-w-0 bg-white rounded-lg",
+            }}
+            {...register("country")}
+          />
+        </MenuHandler>
+        <MenuList className="max-h-[20rem] max-w-[18rem]">
+          {countries.map(({ name, flags }, index) => {
+            return (
+              <MenuItem
+                key={name}
+                value={name}
+                className="flex items-center gap-2"
+                onClick={() => setCountry(index)}
+              >
+                <img
+                  src={flags.svg}
+                  alt={name}
+                  className="h-5 w-5 rounded-full object-cover"
+                />
+                {name}{" "}
+              </MenuItem>
+            );
+          })}
+        </MenuList>
+      </Menu>
+    </div>
+  );
+  const renderStep2 = () => (
+    <div
+      className="mb-1 flex flex-col gap-6"
+    >
+      <Typography
+        variant="h6"
+        color="blue-gray"
+        className="-mb-3 text-white"
+      >
+        Password
+      </Typography>
+      <Input
+        size="lg"
+        placeholder="Input password"
+        className="focus:!border-primary"
+        labelProps={{
+          className: "before:content-none after:content-none",
+        }}
+        containerProps={{
+          className: "bg-white rounded-lg",
+        }}
+        type="password"
+        {...register("password", {required: {value:true, message: 'Password is required'}, minLength: {value:8, message: 'Password must be at least 8 characters long'}, maxLength: {value: 16, message: 'Password must be at most 16 characters long'}})}
+      />
+      {errors.password && <span className="text-red-700">{errors.password.message}</span>}
+      <Typography
+        variant="h6"
+        color="blue-gray"
+        className="-mb-3 text-white"
+      >
+        Repeat password
+      </Typography>
+      <Input
+        size="lg"
+        placeholder="Repeat password"
+        className="focus:!border-primary"
+        labelProps={{
+          className: "before:content-none after:content-none",
+        }}
+        containerProps={{
+          className: "bg-white rounded-lg",
+        }}
+        type="password"
+        {...register("passwordConfirm", {required: {value: true, message: 'Confirm Password is required'}, validate: value => value === password || "Passwords do not match"})}
+      />
+      {errors.passwordConfirm && <span className="text-red-700">{errors.passwordConfirm.message}</span>}
+    </div>
+  );
+  const renderStep3 = () => (
+    <div
+      className="mb-1 flex flex-col gap-6"
+    >
+      <div className="flex justify-between">
+        <p className="text-secondary">Username</p>
+        <p className="text-white">{getValues("username")}</p>
       </div>
-    </main>
-  )
+      <div className="flex justify-between">
+        <p className="text-secondary">Email</p>
+        <p className="text-white">{getValues("email")}</p>
+      </div>
+      <div className="flex justify-between">
+        <p className="text-secondary">Country</p>
+        <p className="text-white">{countryName.name}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-center mb-10 mt-32">
+        <Typography
+          variant="h3"
+          color="blue-gray"
+          className="text-primary"
+        >
+          Super test form
+        </Typography>
+        <Typography
+          variant="h6"
+          color="blue-gray"
+          className="text-secondary"
+        >
+          Initial info
+        </Typography>
+      </div>
+      <div className="grid grid-cols-3">
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-4 items-center">
+            <div
+              className={classNames(
+                "h-5 w-5 rounded-sm",
+                step === 1 ? "bg-primary" : "bg-secondary"
+              )}
+            />
+            <p className="text-secondary">Initial info</p>
+          </div>
+          <div className="flex gap-4 items-center">
+            <div
+              className={classNames(
+                "h-5 w-5 rounded-sm",
+                step === 2 ? "bg-primary" : "bg-secondary"
+              )}
+            />
+            <p className="text-secondary">Password screen</p>
+          </div>
+          <div className="flex gap-4 items-center">
+            <div
+              className={classNames(
+                "h-5 w-5 rounded-sm",
+                step === 3 ? "bg-primary" : "bg-secondary"
+              )}
+            />
+            <p className="text-secondary">Review</p>
+          </div>
+        </div>
+        <div className="w-fit rounded-xl px-5 py-10 bg-[#817CA5] flex flex-col gap-10">
+          <Card color="transparent" shadow={false}>
+            <form
+              className="mt-2 mb-2 max-w-screen-lg sm:w-96"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              {step === 1 && renderStep1()}
+              {step === 2 && renderStep2()}
+              {step === 3 && renderStep3()}
+              <Button type="submit" className={classNames("mt-10 text-primary", Object.keys(errors).length !== 0 ? 'bg-secondary' : 'bg-white' )} fullWidth disabled={Object.keys(errors).length !== 0} >
+                {step === 3 ? 'Complete' : 'Continue'}
+              </Button>
+            </form>
+          </Card>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
